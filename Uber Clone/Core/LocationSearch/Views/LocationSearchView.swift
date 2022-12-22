85cb65a86d2cc7e7455dc1ef9d57335aa745f71d
Michael-Parekh <mparekh4@illinois.edu>
2022-12-21 22:33:20 -0500

    implement location search ui

---
+++ Uber Clone/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,70 @@
+//
+//  LocationSearchView.swift
+//  Uber Clone
+//
+//  Created by Michael Parekh on 12/21/22.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    
+    @State private var startLocationText = ""
+    @State private var destinationLocationText = ""
+    
+    var body: some View {
+        VStack {
+            // H-Stack for the header view (start/destination text inputs).
+            HStack {
+                // Indicator icons for start/destination.
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                
+                // Text input fields in the header.
+                VStack {
+                    TextField("Current location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where to?", text: $destinationLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 64)
+            
+            Divider()
+                .padding(.vertical)
+            
+            // 'ScrollView' for the list view (list of potential addresses).
+            ScrollView {
+                VStack(alignment: .leading) {
+                    // Utilize dummy location cells for now.
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        LocationSearchResultCell()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
